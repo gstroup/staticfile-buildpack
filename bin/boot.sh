78206eed4ea98e6f42735438c93a0e2ca526efbe
Sabith <sabithksme@gmail.com>
2015-03-12 17:45:57 -0700

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -16,8 +16,6 @@
 export APP_ROOT=$HOME
 export LD_LIBRARY_PATH=$APP_ROOT/nginx/lib:$LD_LIBRARY_PATH
 
-echo "CAT" $(cat ./nginx/conf/.urls)
-
 source ./nginx/conf/.urls
 
 conf_file=$APP_ROOT/nginx/conf/nginx.conf
