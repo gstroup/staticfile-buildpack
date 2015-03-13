62d9896f1c7285bbe101ef1b4ffc5b10c44a25e2
Sabith <sabithksme@gmail.com>
2015-03-13 11:34:38 -0700

    Update cf-routes.js

--- bin/cf-routes.js
+++ bin/cf-routes.js
@@ -1,17 +1,20 @@
 'use strict';
 
-window.VCAP_SERVICES = <%= ENV["VCAP_SERVICES"] %>;
+(function(){
+    var VCAP_SERVICES = <%= ENV["VCAP_SERVICES"] %>;
 
-window.getCfRoute = function(serviceName) {
-    for (var serviceId in window.VCAP_SERVICES) {
-        var service = window.VCAP_SERVICES[serviceId];
-        for (var instanceIndex in service) {
-            var instance = service[instanceIndex];
-            if (instance.name === serviceName) {
-                return instance.credentials.url;
+    
+    window.getCfRoute = function(serviceName) {
+        for (var serviceId in VCAP_SERVICES) {
+            var service = VCAP_SERVICES[serviceId];
+            for (var instanceIndex in service) {
+                var instance = service[instanceIndex];
+                if (instance.name === serviceName) {
+                    return instance.credentials.url;
+                }
             }
         }
-    }
-    window.logger.error('Could not find a url for the service name ' + serviceName);
-    return serviceName;
-};
+        window.logger.error('Could not find a url for the service name ' + serviceName);
+        return serviceName;
+    };
+})();
